640042cdb60dacc6118390c7c9081d538f3d27ea
meddil <med.maaoui.007@gmail.com>
2024-04-03 15:28:46 +0000

    remove unuesed code

--- script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('saveLimit').addEventListener('click', () => {
-    const tabLimit = document.getElementById('tabLimit').value;
-    chrome.storage.local.set({ tabLimit }, () => {
-      console.log(`Limit set to ${tabLimit}`);
-    });
-  });
